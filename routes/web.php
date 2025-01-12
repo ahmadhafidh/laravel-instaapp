e825849facd4c2b3c9370526e754668527b6f48d
hafidhayatullah <ahmad.hafidhayatullah@gmail.com>
2025-01-12 15:07:15 +0700

    adding routes, delete unused code adding test/Feature

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,36 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth')->group(function () {
+
+    Route::get('/', [PostController::class, 'listPosts'])->name('home');
+
+    Route::prefix('posts')->name('posts.')->controller(PostController::class)->group(function () {
+        Route::get('', 'listPosts')->name('list');
+        Route::get('create', 'createForm')->name('create');
+        Route::post('create', 'storePost')->name('store');
+
+        Route::get('search', 'searchForm')->name('search.form');
+        Route::get('search/results', 'searchPosts')->name('search.results');
+
+        Route::get('{post}/edit', 'editForm')->name('edit.form');
+        Route::post('{post}', 'updatePost')->name('update');
+
+        Route::get('{post}', 'viewPost')->name('detail');
+        Route::post('{post}/comments', 'storeComment')->name('comment');
+        Route::post('{post}/like', 'toggleLike')->name('like');
+        Route::delete('{post}', 'deletePost')->name('delete');
+    });
+
+    Route::prefix('profile')->name('profile.')->controller(ProfileController::class)->group(function () {
+        Route::get('/', 'viewProfile')->name('view');
+        Route::get('/edit', 'editForm')->name('edit');
+        Route::patch('/update', 'updateProfile')->name('update');
+    });
 });
+
+
+require __DIR__.'/auth.php';
